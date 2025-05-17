4f57414950653ce743d55a3e3796e90cbac3e0d7
mdtofazzalislam <dev.tofazzal122@gmail.com>
2025-05-17 20:28:26 +0600

    explore event handler

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  function handleClick(){
+    alert("button click")
+  }
+
+  const handleClick2 = () => {
+    alert("button 2 click")
+  }
+
+  const handleClick3 = (num) => {
+    alert(num + 5)
+  }
+
+  return (
+    <>
+      <h1>React Core Concepts 2</h1>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me</button>
+      
+      <button onClick={() => handleClick3(3)}>Click Me</button>
+      
+    </>
+  )
+}
+
+export default App
