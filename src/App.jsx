3f4ac50e588ccdc89f781a202758080bb28cd122
mdtofazzalislam <dev.tofazzal122@gmail.com>
2025-05-18 23:42:57 +0600

    useEffect explore

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Counter from './assets/Count'
 import Team from './assets/Team'
+import User from './assets/User'
 
 function App() {
  
@@ -21,6 +22,7 @@ function App() {
   return (
     <>
       <h1>React Core Concepts 2</h1>
+      <User></User>
       <Team></Team>
       <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
